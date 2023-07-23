f34879000c414d9d2478bf8d2993be18a1e94e2c
Kendall <kendallawills@gmail.com>
2023-07-23 15:44:46 -0400

    Final Push

--- main.js
+++ main.js
@@ -106,3 +106,10 @@ console.log(removeElement(concept));
 
 removeElement(concept);
 console.log(concept);
+
+// Nested Arrays
+
+const numberClusters = [[1, 2], [3, 4], [5, 6]];
+
+const target = numberClusters[2][1];
+console.log(target);
