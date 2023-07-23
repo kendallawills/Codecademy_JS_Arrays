0ff6eeab734ddd817473f4a647b33afa8fda15bb
Kendall <kendallawills@gmail.com>
2023-07-23 14:33:21 -0400

    Arrays with let and const

--- main.js
+++ main.js
@@ -29,3 +29,18 @@ let groceryList = ['bread', 'tomatoes', 'milk'];
 groceryList[1] = 'avocados';
 
 console.log(groceryList);
+
+// Arrays with let and const
+
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+console.log(condiments);
+
+condiments = ['Mayo'];
+console.log(condiments);
+
+utensils[3] = 'Spoon';
+console.log(utensils);
